597484fbedf9f5acdf502a61023bee3209c92d78
tambotitree <70440096+tambotitree@users.noreply.github.com>
2025-05-03 10:40:59 +0800

    ./configure ran to completion.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+# Clean up autotools cache directories and files
+echo "Cleaning up autotools cache..."
+rm -rf autom4te.cache
+rm -f config.cache
+
 # -touch build/config.rpath
 aclocal -I m4
 autoreconf --install --force --verbose
